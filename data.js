0a9b17c74cc584e78558cfc420c582ac154c1b75
Britta <brittan.villegas@gmail.com>
2022-11-01 21:51:03 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,35 @@
+export const postContent = [
+    {
+        name: "Dorothea Tanning",
+        username: "istaytanning",
+        location: "Manhatten, NYC",
+        avatar: "images/avatar-dorothea.jpg",
+        post: "images/post-dorothea.jpg",
+        comment: "casual look for a casual monday, nbd",
+        isLiked: false,
+        likes: 15202,
+        uuid: "dorothea"
+    },
+    {
+        name: "Frida Kahlo",
+        username: "fridomfighter",
+        location: "Coyoacán, Ciudad de México, México",
+        avatar: "images/avatar-frida.jpg",
+        post: "images/post-frida.webp",
+        comment: "double trouble",
+        isLiked: false,
+        likes: 22502,
+        uuid: "frida"
+    },
+        {
+        name: "Élisabeth Vigée Le Brun",
+        username: "madamevigee",
+        location: "Paris, France",
+        avatar: "images/avatar-vigee.jpg",
+        post: "images/post-vigee.jpg",
+        comment: "new hat, who dis?",
+        isLiked: false,
+        likes: 10137,
+        uuid: "vigee"
+    }
+]
---
+++ index.js
@@ -0,0 +1,69 @@
+import { postContent } from "/data.js"
+
+renderPosts()
+
+document.addEventListener('click', function(e){
+     if (e.target.dataset.like) {
+         handleLikeClick(e.target.dataset.like)
+     }
+})
+
+function handleLikeClick(postId) {
+    const targetPostObj = postContent.filter(function(post){
+        return post.uuid === postId
+    })[0]
+    
+    if (targetPostObj.isLiked) {
+        targetPostObj.likes--
+        targetPostObj.isLiked = false
+    } else {
+        targetPostObj.likes++
+        targetPostObj.isLiked = true
+    }
+    renderPosts()
+}
+
+function getFeedHtml() {
+    let feedHtml = ``
+    postContent.forEach(function(post) {
+        
+        let likeIconClass = ''
+        if(post.isLiked) {
+            likeIconClass = 'liked'
+        }
+
+        feedHtml += `
+        <article class = "post-framework">
+                <div class="user">
+                    <img class="avatar" src=${post.avatar}>
+                    <div class="user-info">
+                        <p class="name">${post.name}</p>
+                        <p class="location">${post.location}</p>
+                    </div>
+                </div>
+                <div>
+                    <img class="post" src="${post.post}">
+                </div>
+                <div class="icon-section">
+                    <i class="fa-solid fa-heart ${likeIconClass}"
+                    data-like=${post.uuid}></i>
+                    <i class="fa-regular fa-comment"></i>
+                    <i class="fa-regular fa-paper-plane"></i>
+                </div>
+                <div class="caption-section">
+                    <div>
+                        <p id="like-count" class="like-count">${post.likes} likes</p>
+                    </div>
+                    <div>
+                        <p class="caption"><span class="bold">${post.username}</span> ${post.comment}</p>
+                    </div>
+                </div>
+        </article>`
+    })
+    return feedHtml
+}
+
+function renderPosts() {
+    document.getElementById("main").innerHTML = getFeedHtml()
+}
+
